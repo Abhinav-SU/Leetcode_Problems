2f25a62b774869ac2d32a5330d2438afa0aa778a
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2025-10-08 15:37:59 -0700

    Time: 3 ms (11.29%), Space: 48.8 MB (7.24%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,10 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1])
-                profit+=prices[i]-prices[i-1];
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        
+        // Initialize dp with a sentinel value
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
         }
-        return profit;
+        
+        return f(0, 1, prices, dp);
+    }
+
+    public int f(int indx, int buy, int[] prices, int[][] dp) {
+        if (indx == prices.length)
+            return 0;
+        
+        if (dp[indx][buy] != Integer.MIN_VALUE) 
+            return dp[indx][buy];
+        
+        int profit = 0;
+        if (buy == 1) {
+            // Option to buy or skip
+            profit = Math.max(-prices[indx] + f(indx + 1, 0, prices, dp), 
+                              f(indx + 1, 1, prices, dp));
+        } else {
+            // Option to sell or skip
+            profit = Math.max(prices[indx] + f(indx + 1, 1, prices, dp), 
+                              f(indx + 1, 0, prices, dp));
+        }
+        
+        return dp[indx][buy] = profit;
     }
 }
