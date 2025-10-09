c908a4ef079f7f35f8270961f4a8ca40e6af4956
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2025-10-09 11:03:27 -0700

    Time: 3 ms (87.71%), Space: 61 MB (75.88%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,14 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int t1buy = Integer.MAX_VALUE,t2buy =Integer.MAX_VALUE;
-        int t1profit = 0, t2profit =0;
+        int S1_Profit = 0, B1_Profit = Integer.MIN_VALUE; // Transaction 1 states
+    int S2_Profit = 0, B2_Profit = Integer.MIN_VALUE; // Transaction 2 states
+        for (int price : prices) {
 
-        for(int price : prices){
-            t1buy = Math.min(t1buy, price);
-            t1profit = Math.max(t1profit,price-t1buy);
-            t2buy = Math.min(t2buy,price-t1profit);
-            t2profit = Math.max(t2profit,price-t2buy);
-        }
-        return t2profit;
+                    // Transaction 1 updates (must use results from Transaction 0 = 0)
+        S1_Profit = Math.max(S1_Profit, B1_Profit + price); // Sell 1 (based on B1)
+        B1_Profit = Math.max(B1_Profit, -price); 
+        // Transaction 2 updates (must use results from Transaction 1)
+        S2_Profit = Math.max(S2_Profit, B2_Profit + price); // Sell 2 (based on B2)
+        B2_Profit = Math.max(B2_Profit, S1_Profit - price); // Buy 2 (based on S1)
+        
+           // Buy 1 (based on 0 profit)
+    }            
+    return S2_Profit;
     }
 }
