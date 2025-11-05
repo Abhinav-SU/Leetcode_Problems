84c014e98c8c0bf9f8cae79fb97d9e431d75c31f
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2025-11-05 14:48:03 -0800

    Time: 244 ms (62.06%), Space: 20.3 MB (57.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,38 @@
-        
+# Count island using bfs
+from collections import deque
 class Solution:
     
     def numIslands(self,grid):
         
         rows = len(grid)
         cols = len(grid[0])
-        island_count = 0
         
         directions = [(0,1),(0,-1),(1,0),(-1,0)]
         
-        def dfs_sink(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols:
-                return
-            if grid[r][c] == '0':
-                return
+        def bfs(r,c):
             
-            grid[r][c]='0'
             
-            for dr,dc in directions:
-                dfs_sink(r+dr,c+dc)
+            q = deque()
+            q.append((r,c))
+            grid[r][c] = '0'
+            
+            while q:
+                nr , nc = q.popleft()
                 
+                for dr, dc in directions:
+                    nrow, ncol = nr + dr , nc +dc
+                    if 0 <= nrow < rows and 0 <= ncol < cols:
+                        if grid[nrow][ncol] == '1':
+                            grid[nrow][ncol] = '0'
+                            q.append((nrow,ncol))
+        island_count = 0    
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]=='1':
-                    island_count +=1                
-                    dfs_sink(r,c)
+                    island_count+=1
+                    bfs(r,c)
         return island_count
+                    
+        
+            
+            
