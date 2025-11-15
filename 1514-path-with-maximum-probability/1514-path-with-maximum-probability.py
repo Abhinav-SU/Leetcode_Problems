2733e5c2489f2f4d3b6e9035938b29ca84fafe04
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2025-11-15 13:26:09 -0800

    Time: 75 ms (98.29%), Space: 28.1 MB (87.21%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -2,34 +2,36 @@ import heapq
 class Solution:
     def maxProbability(self,n,edges,succProb,start_node,end_node):
         
-        #Convert edges to make a graph
-        graph = [[] for i in range(n)]
+        
+        graph =[[] for _ in range(n)]
         
         for (u,v),w in zip(edges,succProb):
             graph[u].append((v,w))
             graph[v].append((u,w))
             
-        max_prob = [0.0] * n
-        max_prob[start_node] = 1.0
-        
-        pq = [(-1.0,start_node)]
-        
-        while pq:
+        maxProb = [0.0] * n
+        maxProb[start_node] = -1.0
             
-           curr_prob,node =heapq.heappop(pq)
-           curr_prob = -curr_prob
-           
-           if curr_prob < max_prob[node]:
-               continue
-           
-           if node == end_node:
-               return curr_prob
-               
-           for v, edge_prob in graph[node]:
-               newEdge_prob = curr_prob * edge_prob
-               
-               if newEdge_prob > max_prob[v]:
-                   max_prob[v] = newEdge_prob
-                   heapq.heappush(pq,(-newEdge_prob,v))
-        
-        return max_prob[end_node]
+        pq = [(-1.0,start_node)]
+            
+            
+        while pq:
+                
+            curr_prob, curr_node = heapq.heappop(pq)
+            curr_prob = -curr_prob
+                
+            if curr_prob < maxProb[curr_node]:
+                continue
+                
+            if curr_node == end_node:
+                return curr_prob
+                    
+            for v,edge_prob in graph[curr_node]:
+                newEdgeProb = curr_prob * edge_prob
+                    
+                    
+                if newEdgeProb > maxProb[v]:
+                    maxProb[v] = newEdgeProb
+                    heapq.heappush(pq,(-newEdgeProb,v))
+                        
+        return maxProb[end_node]
