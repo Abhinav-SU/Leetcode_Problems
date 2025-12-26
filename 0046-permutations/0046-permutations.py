8866a2ed038285994ecd6b36a18f7e22c8710a45
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2025-12-26 13:03:30 -0800

    Time: 3 ms (33.72%), Space: 17.5 MB (95.9%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,26 +1,22 @@
-
 class Solution:
-    def permute(self,nums):
-        num_len = len(nums)
-        result = []
-        if num_len == 0:
-            return []
-        if num_len == 1:
-            return [list(nums)]
-
-            
-        def dfs(currList):
-            
-            if len(currList) == num_len:
-                result.append(list(currList))
-                return
-                
-            for num in nums:
-                if num not in currList:
-                    currList.append(num)
-                    dfs(currList)
-                    currList.pop()
-                    
-        dfs([])
-        
-        return result 
+	
+	def permute(seld,nums):
+		
+		result =[]
+		
+		def backtrack(path):
+			
+			if len(nums)==len(path):
+				result.append(path[:])
+				return 
+				
+			for num in nums:
+				if num in path:
+					continue
+					
+				path.append(num)
+				backtrack(path)
+				path.pop()
+				
+		backtrack([])
+		return result
