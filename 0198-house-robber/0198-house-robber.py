744289ae3d7d5290a0e59a04b1e2ca740682c268
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-02-24 14:13:01 -0800

    Time: 0 ms (100%), Space: 19.5 MB (21.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,16 +1,14 @@
 class Solution:
 	def rob(self,nums):
-		self.memo = {}
-		return self.rob_top_down(len(nums)-1,nums)
+		n = len(nums)
 		
-	def rob_top_down(self,i,nums):
-		if i < 0: return 0
-		if i == 0: return nums[0]
+		prev1 = 0
+		prev2 = 0
+
 		
-		if i in self.memo:
-			return self.memo[i]
-			
-		res = max(nums[i]+self.rob_top_down(i-2,nums),self.rob_top_down(i-1,nums))
+		for x in nums:
+			curr = max(x+prev2,prev1)
+			prev2 = prev1
+			prev1 = curr
 		
-		self.memo[i] = res
-		return res
+		return prev1
