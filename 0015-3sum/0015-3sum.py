a5d7896353343debee83e6cc76ba0371174a0f5e
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-01 14:00:58 -0700

    Time: 491 ms (89.79%), Space: 22.1 MB (80.92%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,35 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
-        nums.sort()
-        for i in range (0,len(nums)-2):
-            if nums[i]>0:
-                return ans
-            if i>0 and nums[i-1]== nums[i]:
-                continue
-            j,k,target=i+1,len(nums)-1,-nums[i]
-            while j<k:
-                current_sum =nums[j] + nums[k]
-                if  current_sum == target:
-                    ans.append([nums[i],nums[j],nums[k]])
-                    j+=1
-                    k-=1
-                    while j<k and nums[j] == nums[j-1]:
-                        j+=1
-                    while j<k and nums[k] == nums[k+1]:
-                        k-=1
-                elif current_sum > target:
-                    k-=1
-                else:
-                    j+=1
-        return ans
+	def threeSum(self,nums: List[int]) -> List[List[int]]:
+		if not nums:
+			return []
+		nums.sort()# -4,-1,-1,0,1,2
+		n = len(nums)
+		answer = []
+		
+		
+		for i in range(n-2):
+			if nums[i] > 0:
+				break
+			if i != 0 and nums[i-1] == nums[i]:
+				continue
+				
+			j=i+1
+			k=n-1
+			
+			while j < k:
+				current_sum =nums[i] + nums[j] + nums[k]
+				if current_sum==0:
+					answer.append([nums[i],nums[j],nums[k]])
+					while j < k and nums[j] == nums[j+1]:
+						j+=1
+					while j < k and nums[k] == nums[k-1]:
+						k-=1
+						
+					j+=1
+					k-=1
+				elif current_sum < 0:
+					j+=1
+				else:
+					k-=1
+					
+		return answer
