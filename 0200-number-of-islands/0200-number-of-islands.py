8bd34df6b5bcf384c859b93d80befa291ecf5d31
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-06 11:40:58 -0700

    Time: 250 ms (44.93%), Space: 21.6 MB (48.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,28 @@
 class Solution:
-	def numIslands(self,grid: List[List[int]]) -> int:
+	def numIslands(self,grid:List[List[int]])->int:
 		if not grid:
 			return 0
-		m,n =len(grid),len(grid[0])
-		if m==0 and n==0:
-			return 0
-		count =0 
+		m = len(grid)
+		n = len(grid[0])
 		directions = [(0,1),(0,-1),(1,0),(-1,0)]
+		count = 0
+		stack = []
 		
-		def dfs(start_r,start_c):
-			stack =[(start_r,start_c)]
-			grid[start_r][start_c]='0'
-			
-
+		
+		def dfs(start,grid):
+			stack.append(start) 
 			while stack:
 				r,c = stack.pop()
 				for dr,dc in directions:
-					nr,nc = r+dr,c+dc
-					if 0 <= nr < m and 0 <= nc < n and grid[nr][nc]=='1':
+					nr,nc = dr+r,dc+c
+					while 0 <= nr  < m and 0 <= nc < n and grid[nr][nc] == '1':
+						grid[nr][nc] = '0'
 						stack.append((nr,nc))
-						grid[nr][nc]='0'
+						
 		for i in range(m):
 			for j in range(n):
-				if grid[i][j]=='1':
-					dfs(i,j)
+				if grid[i][j] == '1':
 					count+=1
-		return count 
+					dfs((i,j),grid)
+					
+		return count
