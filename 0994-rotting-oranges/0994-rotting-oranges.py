ea75b7c2a58f48f8de32fb3daae7d0fc3d5d37fd
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-24 12:59:20 -0700

    Time: 0 ms (100%), Space: 19.2 MB (95.93%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,42 +1,25 @@
-from collections import deque
 class Solution:
-    def orangesRotting(self,grid):
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        fresh_count = 0
-        minutes = 0
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh=0
         q = deque()
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh_count +=1
-                elif grid[r][c] == 2:
-                    q.append((r,c))
-        
-        if fresh_count == 0:
-            return 0
-            
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        
-        while q and fresh_count > 0:
-            
-            
-            minutes +=1
-            size = len(q)
-            
-            for _ in range(size):
-                r,c = q.popleft()
-                
-                for dr,dc in directions:
-                    nr,nc = r+dr, c+dc
-                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] ==1:
+        dire =[(0,1),(0,-1),(1,0),(-1,0)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    fresh +=1
+                if grid[i][j]==2:
+                    q.append((i,j))
+        time = 0
+        while q and fresh > 0:
+            time +=1
+            for _ in range(len(q)):
+                r,c =q.popleft()
+                for dr,dc in dire:
+                    nr,nc = r+dr,c+dc
+                    if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc]==1:
+                        fresh -=1
                         grid[nr][nc] =2
-                        fresh_count -=1
                         q.append((nr,nc))
-                        
-        if fresh_count == 0:
-            return minutes
-        else:
-            return -1
+        
+        return time if fresh==0 else -1
+
