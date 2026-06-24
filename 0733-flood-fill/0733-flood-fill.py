8409a6c2e39aaad5b583cf340df100f547390204
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-23 19:29:01 -0700

    Time: 0 ms (100%), Space: 19.4 MB (84.23%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,28 +1,20 @@
-from collections import deque
-
-
 class Solution:
-    def floodFill(self, image, sr, sc, color):
-        originalColor = image[sr][sc]
-        if originalColor == color:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        dire = [(0,1),(0,-1),(1,0),(-1,0)]
+        orCol = image[sr][sc]
+        def dfs(sr,sc):
+            stack =[(sr,sc)]
+            image[sr][sc] = color
+            while stack:
+                r,c = stack.pop()
+                for dr,dc in dire:
+                    nr,nc = r+dr,c+dc
+                    if 0 <= nr < len(image) and 0 <= nc < len(image[0]) and image[nr][nc]== orCol:
+                        image[nr][nc]= color
+                        stack.append((nr,nc))
+
             return image
 
-        n, m = len(image), len(image[0])
 
-        q = deque()
+        return dfs(sr,sc) if image[sr][sc] != color else image
 
-        q.append((sr, sc))
-        image[sr][sc] = color
-
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-        while q:
-            r, c = q.popleft()
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < n and 0 <= nc < m:
-                    if image[nr][nc] == originalColor:
-                        image[nr][nc] = color
-                        q.append((nr, nc))
-
-        return image
