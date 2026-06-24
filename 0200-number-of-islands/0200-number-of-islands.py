c46a8bdbb75d8e8fc2add74b9b3a2de5a6dceb3a
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-23 17:46:08 -0700

    Time: 243 ms (60.07%), Space: 21.6 MB (48.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,23 @@
 class Solution:
-	def numIslands(self,grid:List[List[int]])->int:
-		if not grid:
-			return 0
-		m = len(grid)
-		n = len(grid[0])
-		directions = [(0,1),(0,-1),(1,0),(-1,0)]
-		count = 0
-		stack = []
-		
-		
-		def dfs(start,grid):
-			stack.append(start) 
-			while stack:
-				r,c = stack.pop()
-				for dr,dc in directions:
-					nr,nc = dr+r,dc+c
-					while 0 <= nr  < m and 0 <= nc < n and grid[nr][nc] == '1':
-						grid[nr][nc] = '0'
-						stack.append((nr,nc))
-						
-		for i in range(m):
-			for j in range(n):
-				if grid[i][j] == '1':
-					count+=1
-					dfs((i,j),grid)
-					
-		return count
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n,m = len(grid),len(grid[0])
+        dire =[(0,1),(0,-1),(1,0),(-1,0)]
+        count = 0
+
+        def dfs(start_r,start_c):
+            stack = [(start_r,start_c)]
+            grid[start_r][start_c] ='0'
+            while stack:
+                r,c = stack.pop()
+                for dr,dc in dire:
+                    nr,nc = r+dr , c+dc
+                    if 0 <= nr < n and 0 <= nc < m and grid[nr][nc]=='1':
+                        grid[nr][nc] ='0'
+                        stack.append((nr,nc))
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j]=='1':
+                    count +=1
+                    dfs(i,j)
+        return count
