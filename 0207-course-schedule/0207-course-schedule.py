3379f13ae4971568736d992b1ec5cdffc1afee9c
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-24 11:01:07 -0700

    Time: 6 ms (50.66%), Space: 20.3 MB (79.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #create graph #initialize indegree 
+        indeg = [0] * numCourses
         graph = defaultdict(list)
-        indegree = [0] * numCourses
-        for u,v in prerequisites:
-            graph[u].append(v)
-            indegree[v] += 1
 
-        q = deque()
-        q.extend([i for i in range(numCourses) if indegree[i] == 0])
-        #add each node of the graph and check for the node where indegree is 0 
-        #add that to the queue 
-        completed_course = 0
-        #while a node exist in our queue
+        for course,prereq in prerequisites:
+            graph[prereq].append(course)
+            indeg[course] +=1
+
+        q = deque(i for i in range(numCourses) if indeg[i] ==0)
+        count =0
+
         while q:
-            #pop out the queue elem
-            curr_course = q.popleft()
-            #append the node to result 
-            completed_course +=1
-            #now we backtrack to nodes that are connected to this node 
-            for v in graph[curr_course]:
-                indegree[v] -=1
-            # decreaese their indegree by 1 
-                if indegree[v] == 0:
-                    q.append(v)
-            #check that decreased indegree is 0 then add to the queue
-
-        #check if the final result is not = to n if so return false 
-        #else return true
-        return completed_course == numCourses
+            cur_course = q.popleft()
+            count +=1
+            for nxt in graph[cur_course]:
+                indeg[nxt] -=1
+                if indeg[nxt]==0:
+                    q.append(nxt)
+        
+        return count == numCourses
