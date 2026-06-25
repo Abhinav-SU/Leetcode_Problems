6c4d324a0c4cf26ffc76f253ec6bbe67bd089ba8
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-24 23:05:51 -0700

    Time: 7 ms (45.96%), Space: 20.4 MB (63.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -7,15 +7,14 @@ class Solution:
             graph[prereq].append(course)
             indeg[course] +=1
 
-        q = deque(i for i in range(numCourses) if indeg[i] ==0)
-        count =0
-
+        q = deque([i for i in range(numCourses) if indeg[i] == 0])
+        count = 0
         while q:
             cur_course = q.popleft()
-            count +=1
+            count += 1
             for nxt in graph[cur_course]:
                 indeg[nxt] -=1
-                if indeg[nxt]==0:
+                if indeg[nxt] == 0:
                     q.append(nxt)
-        
+
         return count == numCourses
