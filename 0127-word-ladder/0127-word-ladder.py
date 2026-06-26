fdf6c1f4276aefb3ab33053c7e222d6cd56b9128
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-26 09:19:36 -0700

    Time: 28 ms (98.55%), Space: 20.5 MB (66.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,6 +3,41 @@ class Solution:
         wordSet = set(wordList)
         beginSet = {beginWord}
         endSet = {endWord}
+        if endWord not in wordSet:
+            return 0
+        count = 1  
+        while beginSet and endSet:
+            if len(beginSet) > len(endSet):
+                beginSet,endSet = endSet,beginSet
+            newSet = set()
+            for word in beginSet:
+                wordArr = list(word)
+                for i in range(len(wordArr)):
+                    orgChar = wordArr[i]
+                    for c in "abcdefghijklmnopqrstuvwxyz":
+                        wordArr[i] = c
+                        new_word = "".join("wordArr")
+
+                        if new_word == word:
+                            continue
+
+                        if new_word in endSet:
+                            return count + 1
+
+                        if new_word in wordSet:
+                            newSet.add(new_word)
+                            wordSet.remove(new_word)
+                        wordArr[i] = orgChar
+            beginSet = newSet
+            count +=1           
+        return 0
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordSet = set(wordList)
+        beginSet = {beginWord}
+        endSet = {endWord}
 
         if endWord not in wordSet:
             return 0
