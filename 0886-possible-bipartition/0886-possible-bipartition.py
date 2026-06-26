16a7a527a53a515a2c9fa2fd8b8e59a111415d23
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-25 23:20:53 -0700

    Time: 40 ms (39.49%), Space: 22.9 MB (94.85%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,22 +1,31 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        graph = [[] for _ in range(n)]
+        color = [-1] * n
+        graph = defaultdict(list)
+        
         for u,v in dislikes:
             graph[u-1].append(v-1)
             graph[v-1].append(u-1)
 
-        color = [-1] * n
+        # 0-> 1, 3 # 0
+        # 1-> 0, 4 # 1
+        # 2-> 0   # 1
+        # 3-> 1   # 0
+
         for i in range(n):
             if color[i] != -1:
                 continue
-            color[i]=0
-            q=deque([i])
+            color[i] = 0
+            q = deque([i])
             while q:
-                node = q.popleft()
-                for nxt in graph[node]:
+                curr = q.popleft()
+                for nxt in graph[curr]:
                     if color[nxt] == -1:
-                        color[nxt]=1-color[node]
+                        color[nxt] = 1 - color[curr]
                         q.append(nxt)
-                    elif color[nxt] == color[node]:
+                    elif color[curr] == color[nxt]:
                         return False
         return True
+
+            
+                
