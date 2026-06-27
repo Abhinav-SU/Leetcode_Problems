fae40e848129a2eb818ce34aafc85ac2d1fb6cd5
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-27 13:01:37 -0700

    Time: 15 ms (12.21%), Space: 20.3 MB (70.02%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,20 +1,35 @@
+class DSU:
+    def __init__(self,n):
+        self.root = [i for i in range(n)]
+        self.rank = [1] * n
+        self.count = n
+
+    def find(self,x):
+        if self.root[x] == x:
+            return self.root[x]
+        self.root[x] = self.root[self.find(self.root[x])]
+        return self.root[x]
+
+    def union(self,x,y):
+        rootX,rootY = self.find(x),self.find(y)
+
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootY] > self.rank[rootX]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = self.root[rootX]
+                self.rank[rootX] +=1
+            self.count -= 1
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        size = len(isConnected)
-        noComp = 0
-        visit = [False] * size
+        n = len(isConnected)
+        dsu = DSU(n)
 
-        def dfs(i,isConnected,visit):
-            visit[i] = True
-            for c in range(len(isConnected)):
-                if isConnected[i][c] and not visit[c]:
-                    dfs(c,isConnected,visit)
-
-        for i in range(size):
-            if not visit[i]:
-                noComp +=1
-                dfs(i,isConnected,visit)
-
-        return noComp
-
-
+        for u in range(n):
+            for v in range(n):
+                if isConnected[u][v] ==1:
+                    dsu.union(u,v)
+        return dsu.count
