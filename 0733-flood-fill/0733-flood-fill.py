7f0f00b3cb56c055485487415d459b46117afb38
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-27 11:36:49 -0700

    Time: 0 ms (100%), Space: 19.3 MB (96.54%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,18 +1,21 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        orColor = image[sr][sc]
-        if orColor == color:
+        m,n = len(image),len(image[0])
+
+        if image[sr][sc] == color:
             return image
+
         dire = [(0,1),(0,-1),(1,0),(-1,0)]
-        stack =[(sr,sc)]
-        image[sr][sc] =color
+        stack = [(sr,sc)]
+        orgColor = image[sr][sc]
+        image[sr][sc] = color
 
         while stack:
             r,c = stack.pop()
             for dr,dc in dire:
                 nr,nc = r+dr,c+dc
-                if 0 <= nr <len(image) and 0 <= nc < len(image[0]) and image[nr][nc]==orColor:
-                    image[nr][nc]=color
+                if 0 <= nr < m and 0 <= nc < n and image[nr][nc] == orgColor:
+                    image[nr][nc] = color
                     stack.append((nr,nc))
         return image
 
