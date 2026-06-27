150ee9e9e53540133eb12d7c1a8dc77bb30bdbeb
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-26 21:03:20 -0700

    Time: 2158 ms (5.02%), Space: 21.3 MB (44.53%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,45 +1,41 @@
-
 class Solution:
-    def minimumEffortPath(self,heights):
-        
-        
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
         rows = len(heights)
         cols = len(heights[0])
-        target =(rows-1,cols-1)
-
+        dire = [(0,1),(0,-1),(1,0),(-1,0)]
 
         low = 0
         high = 1000000
-        min_effort = high
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-         
+        minEffort = high
+        target =(rows -1, cols -1)
+
+
         def bfs(effort):
-            
+
             q = deque([(0,0)])
-            visited = set([(0,0)])
-            
+            visited =set([(0,0)])
+
             while q:
                 r,c = q.popleft()
-                
                 if (r,c) == target:
                     return True
-                    
-                for dr,dc in directions:
-                    nr,nc = r+dr, c+dc
+                for dr,dc in dire:
+                    nr,nc = r+dr,c+dc
                     if 0 <= nr < rows and 0 <= nc < cols:
-                        step_effort = abs(heights[nr][nc]-heights[r][c])
-                        if step_effort <= effort and (nr,nc) not in visited:
-                            visited.add((nr,nc))
+                        minEffort = abs(heights[r][c] - heights[nr][nc])
+                        if minEffort <= effort and (nr,nc) not in visited:
                             q.append((nr,nc))
-                            
+                            visited.add((nr,nc))
             return False
-                    
-        
-        while low <= high:
-            mid = low + (high-low)//2
+
+
+
+
+        while low <=high:
+            mid = (low+high)//2
             if bfs(mid):
-                min_effort =mid
-                high = mid - 1
+                minEffort = mid
+                high = mid -1
             else:
                 low = mid + 1
-        return min_effort 
+        return minEffort
