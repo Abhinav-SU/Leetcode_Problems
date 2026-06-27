eecd3e9f97647df0d8d3b36d3a26834eb765eb00
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-26 21:20:01 -0700

    Time: 99 ms (5.4%), Space: 19.7 MB (81.04%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,53 +1,44 @@
-# Swim in Rising Water 
-
 class Solution:
-    def swimInWater(self,grid):
-        
-        
-        n = len(grid)
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        dire =[(0,1),(0,-1),(1,0),(-1,0)]
+
         low = 0
         high = 2500
-        min_time =high
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        
-        
+        time = high
+
         def canReach(time):
-            
+
             if grid[0][0] > time:
                 return False
-            
             q = deque([(0,0)])
             visited = set([(0,0)])
-            
+
             while q:
-                
                 r,c = q.popleft()
-                
-                if r == n-1 and c == n-1:
+                if (r,c) == (rows-1,cols-1):
                     return True
-                    
-                for dr,dc in directions:
-                    nr,nc = r+dr, c+dc 
-                    if 0 <= nr < n and 0 <= nc < n and (nr,nc) not in visited:
+                for dr,dc in dire:
+                    nr,nc =r+dr,c+dc
+                    if 0 <= nr < rows and 0 <= nc < cols and (nr,nc) not in visited:
                         cell_val = grid[nr][nc]
                         if cell_val <= time:
-                            visited.add((nr,nc))
                             q.append((nr,nc))
+                            visited.add((nr,nc))
             return False
-            
+
+        
+
+
         while low <= high:
-            mid = low + (high-low)//2
-            
+            mid = (low+high)//2
             if canReach(mid):
-                min_time =mid
-                high = mid -1
+                time = mid
+                high = mid - 1
             else:
-                low = mid +1
-                
-        return min_time
-                        
-            
-        
-        
-        
-        
+                low = mid + 1
+
+        return time
+
