7dbb4090d37bca3ee5ea39a9f311eeddb1174d07
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-27 13:19:54 -0700

    Time: 2 ms (89.61%), Space: 20.3 MB (93.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,20 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indeg = [0] * numCourses
+        indeg = [0]*numCourses
         graph = defaultdict(list)
 
-        for course,prereq in prerequisites:
+        for course, prereq in prerequisites:
             graph[prereq].append(course)
             indeg[course] +=1
 
-        q = deque([i for i in range(numCourses) if indeg[i] == 0])
-        count = 0
+        q = deque([i for i in range(numCourses) if indeg[i]==0])
+        count =0
+
         while q:
-            cur_course = q.popleft()
-            count += 1
-            for nxt in graph[cur_course]:
+            curr_course = q.popleft()
+            count +=1
+            for nxt in graph[curr_course]:
                 indeg[nxt] -=1
                 if indeg[nxt] == 0:
                     q.append(nxt)
 
-        return count == numCourses
+        return count == numCourses 
+
