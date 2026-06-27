2dc1f9e077e3cc8250f4c382135bad56f64531f8
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-06-26 21:33:00 -0700

    Time: 242 ms (62.57%), Space: 21.4 MB (91.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        n,m = len(grid),len(grid[0])
+        m,n = len(grid),len(grid[0])
         dire =[(0,1),(0,-1),(1,0),(-1,0)]
-        count = 0
+        count =0
 
-        def dfs(start_r,start_c):
-            stack = [(start_r,start_c)]
-            grid[start_r][start_c] ='0'
+        def dfs(r,c):
+            stack =[(r,c)]
             while stack:
-                r,c = stack.pop()
+                sr,sc = stack.pop()
+                grid[sr][sc] = 0
                 for dr,dc in dire:
-                    nr,nc = r+dr , c+dc
-                    if 0 <= nr < n and 0 <= nc < m and grid[nr][nc]=='1':
-                        grid[nr][nc] ='0'
+                    nr,nc = sr+dr,sc+dc
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc]=='1':
                         stack.append((nr,nc))
+                    
 
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j]=='1':
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
                     count +=1
                     dfs(i,j)
-        return count
+        return count 
