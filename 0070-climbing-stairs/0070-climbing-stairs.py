f9669bdc3c235d61d30aca896c761035c178e1ba
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-07-06 09:06:30 -0700

    Time: 0 ms (100%), Space: 19.2 MB (85.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,13 @@
 class Solution:
-	def climbStairs(self,n):
-		prev1 =1 #no of ways to reach 1st stair
-		prev2 =2 #no of ways to reach 2nd stair 
-		count =0
-		if n==1:
-			return prev1
-		elif n==2:
-			return prev2
-		else:
-			for i in range(3,n+1):
-				count = prev1 + prev2
-				prev1 = prev2
-				prev2 = count
-				
-		return count
+    def climbStairs(self, n: int) -> int:
+        a= 1
+        b=2
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        for i in range(1,n-1):
+            res = a+b
+            a =b
+            b = res
+        return res
