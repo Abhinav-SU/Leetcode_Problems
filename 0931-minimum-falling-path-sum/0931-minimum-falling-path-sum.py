29f269b13d3a3c38adb7622499beed000e4d8801
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-07-13 12:22:55 -0700

    Time: 17 ms (57.74%), Space: 20.1 MB (59.73%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -3,25 +3,16 @@ class Solution:
         n = len(matrix)
         dp = [[float('inf')] * n for _ in range(n)]
         
-        def f(i,j):
-            if i < 0 or i>=n:
-                return float('inf')
-            if j < 0 or j>=n:
-                return float('inf')
-            if i==0:
-                return  matrix[i][j]
-            if dp[i][j] != float('inf'):
-                return dp[i][j]
-            up = f(i-1,j)
-            ldg = f(i-1,j-1)
-            rdg = f(i-1,j+1)
+        for j in range(n):
+            dp[0][j] = matrix[0][j]
             
-            dp[i][j] = matrix[i][j] + min(up,min(ldg,rdg))
-            return dp[i][j]
-            
-        mini = float('inf')
-        
-        for i in range(n):
-            mini = min(mini,f(n-1,i))
-            
-        return mini
+        for i in range(1,n):
+            for j in range(n):
+                
+                up  = dp[i-1][j]
+                ldf = dp[i-1][j-1] if j > 0 else float('inf')
+                rdf = dp[i-1][j+1] if j < n-1 else float('inf')
+                
+                dp[i][j] = matrix[i][j] + min(up,ldf,rdf)
+                
+        return min(dp[-1])
