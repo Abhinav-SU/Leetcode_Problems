cb489f464e80d0696252dcfe4ff22b435914c329
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-07-31 16:01:49 -0700

    Time: 23 ms (99.53%), Space: 20.5 MB (73.27%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,72 +1,53 @@
-class Solution:
-    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordSet = set(wordList)
-        beginSet = {beginWord}
-        endSet = {endWord}
-        if endWord not in wordSet:
-            return 0
-        count = 1  
-        while beginSet and endSet:
-            if len(beginSet) > len(endSet):
-                beginSet,endSet = endSet,beginSet
-            newSet = set()
-            for word in beginSet:
-                wordArr = list(word)
-                for i in range(len(wordArr)):
-                    orgChar = wordArr[i]
-                    for c in "abcdefghijklmnopqrstuvwxyz":
-                        wordArr[i] = c
-                        new_word = "".join("wordArr")
-
-                        if new_word == word:
-                            continue
-
-                        if new_word in endSet:
-                            return count + 1
-
-                        if new_word in wordSet:
-                            newSet.add(new_word)
-                            wordSet.remove(new_word)
-                        wordArr[i] = orgChar
-            beginSet = newSet
-            count +=1           
-        return 0
-
+ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        # FIX 1: Convert list to set properly
         wordSet = set(wordList)
-        beginSet = {beginWord}
-        endSet = {endWord}
-
+        
+        # Check early to save time
         if endWord not in wordSet:
             return 0
+            
+        endSet = {endWord}
+        beginSet = {beginWord}
         count = 1
-        while beginSet and endSet:
-
+        
+        while endSet and beginSet:
+            # Swap to always expand the smaller set
             if len(beginSet) > len(endSet):
-                beginSet,endSet = endSet,beginSet
+                beginSet, endSet = endSet, beginSet
+            
+            # FIX 2: newSet and the loops must be OUTSIDE the swap 'if' block
             newSet = set()
+            
             for word in beginSet:
                 wordArr = list(word)
                 for i in range(len(wordArr)):
                     orgChar = wordArr[i]
-                    for c in "abcdefghijklmnopqrstuvwxyz":
+                    
+                    for c in ALPHABETS:
                         wordArr[i] = c
+                        # FIX 3: Remove quotes around wordArr
                         newWord = "".join(wordArr)
-
+                        
                         if newWord == word:
                             continue
-                            
+                        
+                        # Did the ripples touch?
                         if newWord in endSet:
                             return count + 1
-
+                            
+                        # Is it a valid stepping stone?
                         if newWord in wordSet:
                             newSet.add(newWord)
                             wordSet.remove(newWord)
+                            
+                    # Backtrack to the original character for the next iteration
                     wordArr[i] = orgChar
+            
+            # Move to the next level
             beginSet = newSet
-            count +=1
-
+            count += 1
+            
         return 0
-                    
