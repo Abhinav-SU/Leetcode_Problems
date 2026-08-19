1e980267918fd4879bee315058f29be0ee1446e8
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-08-19 15:53:29 -0700

    Time: 0 ms (100%), Space: 20.4 MB (40.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         #the idea is to have two such number from nums array that they result in target
         #to do so we need a hash,map and for eaxch element starting first index we try to find if the target - nums[index] is already present in our hashmap if so we return the asnwre as we also store the index in that hashmap
 
-        numMap = defaultdict()
+        numMap = {}
         for indx in range(len(nums)):
             elem = target - nums[indx]
             if elem in numMap:
