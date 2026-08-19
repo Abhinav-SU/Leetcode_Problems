a4c16198d77d1c52798e890d00b3775c931671dc
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-08-19 15:53:17 -0700

    Time: 0 ms (100%), Space: 20.6 MB (6.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
-	def twoSum(self,nums,target):
-		
-		prevMap ={}
-		
-		for idx,num in enumerate(nums):
-			complement = target - num
-			if complement in prevMap:
-				return [prevMap[complement],idx]
-			
-			prevMap[num] = idx
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #the idea is to have two such number from nums array that they result in target
+        #to do so we need a hash,map and for eaxch element starting first index we try to find if the target - nums[index] is already present in our hashmap if so we return the asnwre as we also store the index in that hashmap
+
+        numMap = defaultdict()
+        for indx in range(len(nums)):
+            elem = target - nums[indx]
+            if elem in numMap:
+                return [numMap.get(elem),indx]
+            numMap[nums[indx]] = indx 
+        return []
