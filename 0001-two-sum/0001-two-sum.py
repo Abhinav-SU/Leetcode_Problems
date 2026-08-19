f7de0c50c3d35c32fa8105c6d1f795ea3c91fdb1
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-08-19 16:00:39 -0700

    Time: 3 ms (53.62%), Space: 20.5 MB (17.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,10 +3,10 @@ class Solution:
         #the idea is to have two such number from nums array that they result in target
         #to do so we need a hash,map and for eaxch element starting first index we try to find if the target - nums[index] is already present in our hashmap if so we return the asnwre as we also store the index in that hashmap
 
-        numMap = {}
-        for indx in range(len(nums)):
-            elem = target - nums[indx]
-            if elem in numMap:
-                return [numMap.get(elem),indx]
-            numMap[nums[indx]] = indx 
+        numsMap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numsMap:
+                return [numsMap[complement],i]
+            numsMap[nums[i]] = i
         return []
