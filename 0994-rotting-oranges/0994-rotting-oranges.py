007de96695f80e1abdeee38da19a961220846b69
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-09-01 11:50:58 -0700

    Time: 4 ms (36.1%), Space: 19.4 MB (41.83%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,29 +1,29 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        m,n = len(grid),len(grid[0])
-        time =0
-        dire = [(0,1),(0,-1),(1,0),(-1,0)]
-        fresh = 0
+    def orangesRotting(self,grid):
+        if not grid:
+            return -1
+        row, col = len(grid),len(grid[0])
+        fresh_count =0
+        DIRECTIONS = [(0,1),(0,-1),(1,0),(-1,0)]
         q = deque()
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==2:
-                    q.append((i,j))
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j]==1:
-                    fresh +=1
-
-        while q and fresh > 0:
-            
-            for _ in range(len(q)):
-                r,c = q.popleft()
-                
-                for dr,dc in dire:
-                    nr,nc = r+dr,c+dc
-                    if 0 <= nr <m and 0 <= nc <n and grid[nr][nc]==1:
-                        grid[nr][nc]=2
-                        fresh -=1
-                        q.append((nr,nc))
+                    fresh_count +=1
+                elif grid[i][j] ==2:
+                    q.append((i,j))
+        time =0
+        while q and fresh_count > 0:
             time +=1
-        return time if fresh==0 else -1
-
-                    
+            for _ in range(len(q)):
+                curRow, curCol = q.popleft()
+                
+                for dr,dc in DIRECTIONS:
+                    nr,nc = curRow + dr , curCol + dc
+                    if 0 <= nr < row and 0 <= nc < col and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh_count -=1
+                        q.append((nr,nc))
+            
+            
+        return time if fresh_count == 0 else -1 
