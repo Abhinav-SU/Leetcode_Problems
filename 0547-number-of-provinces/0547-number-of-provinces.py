51a83fe0081e87b6a64a5521cfd8ae60e1e84288
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-09-05 18:57:17 -0700

    Time: 12 ms (13.67%), Space: 20.3 MB (70.52%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,35 +1,34 @@
 class DSU:
-    def __init__(self,n):
-        self.root = [i for i in range(n)]
-        self.rank = [1] * n
-        self.count = n
-
+    def __init__(self,size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+        self.count = size
     def find(self,x):
-        if self.root[x] == x:
-            return self.root[x]
-        self.root[x] = self.root[self.find(self.root[x])]
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
-
     def union(self,x,y):
-        rootX,rootY = self.find(x),self.find(y)
-
+        rootX = self.find(x)
+        rootY = self.find(y)
         if rootX != rootY:
             if self.rank[rootX] > self.rank[rootY]:
                 self.root[rootY] = rootX
             elif self.rank[rootY] > self.rank[rootX]:
                 self.root[rootX] = rootY
             else:
-                self.root[rootY] = self.root[rootX]
+                self.root[rootY] = rootX
                 self.rank[rootX] +=1
-            self.count -= 1
-
+            self.count -=1
+    def get_count(self):
+        return self.count
+            
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        dsu = DSU(n)
-
-        for u in range(n):
-            for v in range(n):
-                if isConnected[u][v] ==1:
-                    dsu.union(u,v)
-        return dsu.count
+    def findCircleNum(self,isConnected)->int:
+        dsu = DSU(len(isConnected))
+        row,col = len(isConnected),len(isConnected[0])
+        for i in range(row):
+            for j in range(col):
+                if isConnected[i][j] ==1 and i!=j:
+                    dsu.union(i,j)
+        return dsu.get_count()
+                    
