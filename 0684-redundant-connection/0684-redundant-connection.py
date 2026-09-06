f230294adbd018150e6b0d29b47f221514609890
Abhinav <164908303+Abhinav-SU@users.noreply.github.com>
2026-09-05 17:28:04 -0700

    Time: 3 ms (60.93%), Space: 19.7 MB (27.84%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,20 +1,16 @@
 class DSU:
-    def __init__(self,n):
-        self.root = [i for i in range(n)]
-        self.rank = [1] * n
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
 
-    def find(self,x):
-        if self.root[x] == x:
-            return self.root[x]
-        self.root[x] = self.root[self.find(self.root[x])]
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
 
-    def union(self,x,y):
-        rootX,rootY = self.find(x), self.find(y)
-
-        if rootX == rootY:
-            return False
-        
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
         if rootX != rootY:
             if self.rank[rootX] > self.rank[rootY]:
                 self.root[rootY] = rootX
@@ -24,15 +20,12 @@ class DSU:
                 self.root[rootY] = rootX
                 self.rank[rootX] += 1
 
-        return True
-
 
 class Solution:
-    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        dsu = DSU(len(edges)+1)
-
-        for u,v in edges:
-            if not dsu.union(u,v):
-                return [u,v]
-        return []
-            
+    def findRedundantConnection(self, edges):
+        n = len(edges)
+        dsu = DSU(n + 1)
+        for a, b in edges:
+            if dsu.find(a) == dsu.find(b):
+                return [a, b]
+            dsu.union(a, b)
